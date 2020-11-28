69049dfd8c1472aa5f084a932efecc5456f0a666
faraz-karim <73609328+faraz-karim@users.noreply.github.com>
2020-11-28 17:20:38 +0500

    Add files via upload

---
+++ 40%usecases/login.php
@@ -0,0 +1,78 @@
+<?php
+
+
+session_start();
+
+    $Username = $_POST["username"];
+    $password = $_POST['pwd'];
+
+    $path = "";
+
+    if($_SESSION["username"] == "buyer")
+    {
+        $sql = "select * from buyer where Buyer_Username = '$Username'";
+        $path = "Location:Buyer_dashboard.html";
+    }
+    if($_SESSION["username"] == "seller")
+    {
+        $sql = "select * from seller where Seller_Username = '$Username'";
+        $path = "Location:Seller_updateinfo.html";
+    }
+    if($_SESSION["username"] == "admin")
+    {
+        $sql = "select * from administrator where Admin_Username = '$Username'";
+        $path = "Location:Admin_dashboard.html";
+    }
+    
+
+
+    $conn = new mysqli('localhost', 'root', '', 'chuddybase');
+    if($conn->connect_error)
+    {
+        die('Connection failed : ' .$conn->connect_error);
+    }
+    else{
+
+        
+        $result = mysqli_query($conn, $sql);
+ 
+        if(mysqli_num_rows($result) == 0)
+        {
+
+        echo " invalid username ";
+
+        $conn->close();
+
+
+
+        }
+        else{
+            $row = mysqli_fetch_assoc($result);
+            //$sql2 = "select Password from buyer where Buyer_Username = '$Username'";
+            $mypass = $row['Password'] ;
+            
+            // $result2 = mysqli_query($conn, $sql2);
+
+
+            if ($mypass == $password)
+            {
+                if ($_SESSION["username"] == "buyer")
+                {
+                    $_SESSION["buyer_class"] = $row['Buyer_Class'] ;
+                    if ($row['Buyer_Class'] == "Premium" )
+                    {$path = "Location:Buyer_premium_dashboard.html";}
+                }
+                echo " LOGIN SUCCESSFUL!!!!";
+                $_SESSION["actualusername"] = $Username;
+                header($path);
+                
+            }
+            else {
+                echo"PASSWORD INVALID";
+            }
+
+        }
+    }
+
+
+?>
---
+++ 40%usecases/viewartwork.php
@@ -0,0 +1,65 @@
+<?php
+
+$art_id = $_GET["a_id"];
+
+echo "This is the art id you selected " ;
+echo  $art_id;
+echo "<br>";
+
+//$sql = "select * from artwork where Artwork_ID = '$art_id'";
+
+$sql = "SELECT `Artwork_ID`, `Price`,`Year`, artwork.Painting_Style, `Description`, Full_Name, F_name, L_name FROM `artwork`INNER JOIN artist ON artwork.Artist_ID = artist.Artist_ID INNER JOIN seller ON artwork.Seller_Username = seller.Seller_Username WHERE Artwork_ID = \"$art_id\"";
+
+$conn = new mysqli('localhost', 'root', '', 'chuddybase');
+if($conn->connect_error)
+{
+    die('Connection failed : ' .$conn->connect_error);
+}
+else{
+
+    
+    $result = mysqli_query($conn, $sql);
+
+    if(mysqli_num_rows($result) == 0)
+    {
+
+    echo " Error fetching details of this seller ";
+
+    $conn->close();
+
+    
+
+    }
+    else{
+        $row = mysqli_fetch_assoc($result);
+
+            echo "Artwork ID          :     " . $row["Artwork_ID"];
+            echo "<br>";
+
+            echo "Seller Name          :     " .$row["F_name"].$row["L_name"];
+            echo "<br>";
+
+            echo "Artist Name          :     " .$row["Full_Name"];
+            echo "<br>";
+
+            echo "Price          :     " .$row["Price"];
+            echo "<br>";
+
+            echo "Year          :     " .$row["Year"];
+            echo "<br>";
+
+            echo "Painting style          :     " .$row["Painting_Style"];
+            echo "<br>";
+
+            echo "Description          :     " .$row["Description"];
+            echo "<br>";
+            
+            echo "Click here";
+            
+            echo "<br>";
+
+    }
+}
+
+
+?>
